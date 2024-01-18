1ebe7157d7f40b4925ab84361755302f7b6bed39
Lemibuca <146123937+Lemibuca@users.noreply.github.com>
2024-01-18 13:32:36 -0500

    my information v2

--- myInfo.py
+++ myInfo.py
@@ -5,3 +5,4 @@
 print('My name is: Leslie Bustamante')
 print('My address is: 9 Crescent Ave, Totowa, NJ, 07512')
 print('My phone number is: 9082757751')
+print('My college major is: Software Engineering')
