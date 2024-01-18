047d79aef652315cce3c3197deda5c2ce1295853
Lemibuca <146123937+Lemibuca@users.noreply.github.com>
2024-01-18 12:56:14 -0500

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+# CS175L
+#Your first name and Last name
+#myInfo
+
+print('My name is: Leslie Bustamante')
+print('My address is: 9 Crescent Ave, Totowa, NJ, 07512')
+print('My phone number is: 9082757751')
