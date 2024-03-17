6bf94278e8d98bd6c57147a025e4c678cf0f5749
Lemibuca <146123937+Lemibuca@users.noreply.github.com>
2024-03-16 20:49:06 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,21 +1,23 @@
 # CS-175L-02
 # Leslie Bustamante
 # Average from input
+def main():
+    file = open("numbers.txt", "r")
+    lines = file.readlines()
+    k = 0
+    numbers = []
+    for line in lines:
+        line = line.split()
+        for num in line:
+            numbers.append(int(num))
+            k += int(num)
+    average = k/len(numbers)
 
-file = open("numbers.txt", "r")
-lines = file.readlines()
-k = 0
-numbers = []
-for line in lines:
-    line = line.split()
-    for num in line:
-        numbers.append(int(num))
-        k += int(num)
-average = k/len(numbers)
+    print(f'I read in 1 number(s)  Current number is:    {numbers[0]:.2f}        Total is :     {numbers[0]:.2f}')
+    print(f'I read in 2 number(s)  Current number is:    {numbers[1]:.2f}        Total is :     {numbers[1]+numbers[0]:.2f}')
+    print(f'I read in 3 number(s)  Current number is:    {numbers[2]:.2f}       Total is :    {numbers[2]+numbers[1]+numbers[0]:.2f}')
+    print('Average is:',average)
 
-print(f'I read in 1 number(s)  Current number is:    {numbers[0]:.2f}        Total is :     {numbers[0]:.2f}')
-print(f'I read in 2 number(s)  Current number is:    {numbers[1]:.2f}        Total is :     {numbers[1]+numbers[0]:.2f}')
-print(f'I read in 3 number(s)  Current number is:    {numbers[2]:.2f}       Total is :    {numbers[2]+numbers[1]+numbers[0]:.2f}')
-print('Average is:',average)
-
-file.close()
+    file.close()
+if __name__ == '__main__':
+    main()
