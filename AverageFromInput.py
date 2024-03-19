184aa70968c44917c77a0db81f19e750e03e40b3
Lemibuca <146123937+Lemibuca@users.noreply.github.com>
2024-03-18 23:52:46 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,22 +2,23 @@
 # Leslie Bustamante
 # Average from input
 def main():
-    file = open("numbers.txt", "r")
-    lines = file.readlines()
-    k = 0
-    numbers = []
-    for line in lines:
-        line = line.split()
-        for num in line:
-            numbers.append(int(num))
-            k += int(num)
-    average = k/len(numbers)
+      
+    file = open('numbers.txt', 'r')
+    
+    sum = 0
+    count = 0
 
-    print(f'I read in 1 number(s)  Current number is:    {numbers[0]:.2f}        Total is :     {numbers[0]:.2f}')
-    print(f'I read in 2 number(s)  Current number is:    {numbers[1]:.2f}        Total is :     {numbers[1]+numbers[0]:.2f}')
-    print(f'I read in 3 number(s)  Current number is:    {numbers[2]:.2f}       Total is :    {numbers[2]+numbers[1]+numbers[0]:.2f}')
-    print('Average is:',average)
+    for number in file:
+        sum += int(number)
+        count += 1
+        num = int(number)
+        print(f'I read in {count} number(s)  Current number is:    {num:.2f}        Total is :     {sum:.2f} ')
+   
+    average = sum / count
+
+    print('The average is:', average)
 
     file.close()
 if __name__ == '__main__':
     main()
+
