04171b51437c9ec8539161c7e916869a80164273
Lemibuca <146123937+Lemibuca@users.noreply.github.com>
2024-03-23 16:33:58 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,22 +2,31 @@
 # Leslie Bustamante
 # Average from input
 def main():
-      
-    file = open('numbers.txt', 'r')
     
-    sum = 0
-    count = 0
-
-    for number in file:
-        sum += int(number)
-        count += 1
-        num = int(number)
-        print(f'I read in {count} number(s)  Current number is:    {num:.2f}        Total is :     {sum:.2f} ')
+    try:   
+        file = open('numbers.txt', 'r')
+        
+        sum = 0
+        count = 0
+        
+        for number in file:
+            
+            try:
+                sum += int(number)
+                count += 1
+                num = int(number)
+                print(f'I read in {count} number(s)  Current number is:    {num:.2f}        Total is :     {sum:.2f} ')       
+            except ValueError:
+                print(f'Bad data: {number}, skipping')
+       
+            average = sum / count
    
-    average = sum / count
-
-    print('The average is:', average)
-
+        print('The average is:', average)
+        
+    except IOError:
+         print("SystemExit: File not found: numbers.txt - existing")
+        
+    
     file.close()
 if __name__ == '__main__':
     main()
